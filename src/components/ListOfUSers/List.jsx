6b942bb84727dc1ec5162df93d9bc48165a17ca5
andrey <petlyashenko@ukr.net>
2022-01-22 18:15:12 +0200

    1

--- src/components/ListOfUSers/List.jsx
+++ src/components/ListOfUSers/List.jsx
@@ -6,19 +6,19 @@ import { UserContext } from "./UserContext"
 const List = ({ selectedUsersIsShown, filterOptions }) => {
     const [users, setUsers] = useContext(UserContext)
 
-    let storedUsers;
+   
 
-    const addInfoToLocalStorage = () => {
+   
         // localStorage.clear();
-        storedUsers = JSON.parse(localStorage.getItem("selectedUsers"));
+      let  storedUsers = JSON.parse(localStorage.getItem("selectedUsers"));
+        console.log(storedUsers, 'storedUSERS')
         if (localStorage.getItem("selectedUsers") == undefined) {
             localStorage.setItem("selectedUsers", JSON.stringify([]))
         }
-        users.selectedUsers.map((user) => !storedUsers?.includes(user) && storedUsers?.push(user))
+        users.selectedUsers.map((user) => !storedUsers.includes(user) && storedUsers.push(user))
         localStorage.setItem("selectedUsers", JSON.stringify(storedUsers))
-    }
-    addInfoToLocalStorage()
-
+    
+   
     const clearHandler = () => {
         localStorage.setItem("selectedUsers", JSON.stringify([]))
         setUsers(() => ({ ...users, selectedUsers: [] }))
@@ -27,13 +27,13 @@ const List = ({ selectedUsersIsShown, filterOptions }) => {
     const filteredUsers = (users) => {
         return users.filter((i) => {
             if (filterOptions.id && filterOptions.login) {
-                return filterOptions.id == i.id && i.login?.includes(filterOptions.login)
+                return filterOptions.id == i.id && i.login.includes(filterOptions.login)
             }
             if (filterOptions.id) {
                 return filterOptions.id == i.id
             }
             if (filterOptions.login) {
-                return i.login?.includes(filterOptions.login)
+                return i.login.includes(filterOptions.login)
             }
             return i
         })
@@ -44,10 +44,10 @@ const List = ({ selectedUsersIsShown, filterOptions }) => {
             {
                 selectedUsersIsShown ?
                     <div className="listContainer selectedListContainer" >
-                        {filteredUsers(users.data).filter((i) => storedUsers?.includes(i.id)).
+                        {filteredUsers(users.data).filter((i) => storedUsers.includes(i.id)).
                             map(user => <Card user={user} key={user.id} selected={true} />)}
 
-                        <button onClick={clearHandler}>Clear the list</button>
+                        {storedUsers[0] && <button onClick={clearHandler}>Clear the list</button>}
                     </div>
                     :
                     <div className="listContainer">
